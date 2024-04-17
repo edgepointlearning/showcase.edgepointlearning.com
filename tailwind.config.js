98aac42c59635876b5c7cb3f41f2d1602d8f72ba
tylercampbell <tylercampbell@users.noreply.github.com>
2024-04-17 13:39:22 -0400

    initial styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,4 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./src/**/*.{html,njk,css}'],
-
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans':  ['Nunito', ...defaultTheme.fontFamily.sans],
-      },
-    }
-  }
 }
